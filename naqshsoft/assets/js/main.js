4df632cd6236c4e3b73a5433ad0d26e78c12291f
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-01-06 01:16:35 +0500

    Updating full project

--- naqshsoft/assets/js/main.js
+++ naqshsoft/assets/js/main.js
@@ -1,27 +1,41 @@
-window.addEventListener("scroll", () => {
-    let header = document.querySelector('header')
-    header.classList.toggle("scrolled", window.scrollY > 0)
-
-})
-
 let header_burger = document.querySelector(".header_burger"),
     lines = document.querySelectorAll(".header_line"),
+    body = document.querySelector("body"),
+    html = document.querySelector("html"),
     header_item = document.querySelector('.header_item:nth-child(2)')
 header_burger.addEventListener("click", () => {
     header_item.classList.toggle("open")
     lines.forEach((line) => {
         line.classList.toggle("open")
     });
+    body.classList.toggle("opened")
+    html.classList.toggle("opened")
 })
 let header_btn = document.querySelector(".header_btn"),
     header_form = document.querySelector(".header_form"),
-    header_closer = document.querySelector(".header_closer")
+    header_closer = document.querySelector(".header_closer"),
+    from_bg_closer = document.querySelector(".from_bg_closer")
+from_bg_closer.addEventListener("click", () => {
+    header_form.classList.remove("open")
+    body.classList.remove("opened")
+    html.classList.remove("opened")
+    from_bg_closer.classList.remove("opened")
+})
 header_closer.addEventListener("click", () => {
     header_form.classList.remove("open")
+    body.classList.remove("opened")
+    html.classList.remove("opened")
+    from_bg_closer.classList.remove("opened")
 })
-header_btn.addEventListener("click", () => {
-    header_form.classList.add("open")
-})
+let btns = document.querySelectorAll(".btn_form_opener")
+btns.forEach((el) => {
+    el.addEventListener("click", () => {
+        header_form.classList.add("open")
+        body.classList.add("opened")
+        html.classList.add("opened")
+        from_bg_closer.classList.add("opened")
+    })
+});
 
 let header_message_success_closer = document.querySelector(".header_message_success_closer"),
     header_message_error_closer = document.querySelector(".header_message_error_closer")
@@ -46,5 +60,96 @@ header_inner_items.forEach((elem) => {
         lines.forEach((line) => {
             line.classList.toggle("open")
         });
+        body.classList.toggle("opened")
+        html.classList.toggle("opened")
     })
+})
+
+window.addEventListener("load", () => {
+    let loader = document.querySelector(".loader")
+    loader.classList.toggle("loaded")
+    time = setTimeout(displayNoneLoader, 500)
+    function displayNoneLoader() {
+        loader.classList.toggle("closed")
+        loader.classList.toggle("loaded")
+    }
+})
+
+
+let item_works = document.querySelectorAll('.section_works_inner_item'),
+    item_works_spans = document.querySelectorAll('.section_work_span'),
+    section_work_loader = document.querySelector(".section_work_loader")
+
+let web = document.querySelectorAll(".web")
+let mobile = document.querySelectorAll(".mobile")
+let desktop = document.querySelectorAll(".desktop")
+
+let section_works_item_work = document.querySelectorAll(".section_works_item_work")
+
+item_works_spans.forEach(el => {
+    el.addEventListener("click", () => {
+        item_works.forEach((items) => {
+            items.classList.remove("choosed")
+        });
+
+        section_work_loader.classList.toggle("opened")
+        time = setTimeout(displayNoneLoader, Math.round(Math.random() * 1000))
+        function displayNoneLoader() {
+            section_work_loader.classList.toggle("opened")
+        }
+        el.parentElement.classList.add("choosed")
+
+        if (el == item_works_spans[1]) {
+            web.forEach((element) => {
+                element.classList.remove("closed")
+            });
+            desktop.forEach((element) => {
+                element.classList.add("closed")
+            });
+            mobile.forEach((element) => {
+                element.classList.add("closed")
+            });
+        }
+
+        else if (el == item_works_spans[2]) {
+            web.forEach((element) => {
+                element.classList.add("closed")
+            });
+            desktop.forEach((element) => {
+                element.classList.add("closed")
+            });
+            mobile.forEach((element) => {
+                element.classList.remove("closed")
+            });
+        }
+
+        else if (el == item_works_spans[3]) {
+            web.forEach((element) => {
+                element.classList.add("closed")
+            });
+            desktop.forEach((element) => {
+                element.classList.remove("closed")
+            });
+            mobile.forEach((element) => {
+                element.classList.add("closed")
+            });
+        }
+
+        else if (el == item_works_spans[0]) {
+            web.forEach((element) => {
+                element.classList.remove("closed")
+            });
+            desktop.forEach((element) => {
+                element.classList.remove("closed")
+            });
+            mobile.forEach((element) => {
+                element.classList.remove("closed")
+            });
+        }
+    })
+});
+
+let btn_up = document.querySelector(".btn-up")
+window.addEventListener("scroll", () => {
+    btn_up.classList.toggle("opened", window.scrollY > 500)
 })
--- naqshsoft/assets/js/swiper_settings.js
+++ naqshsoft/assets/js/swiper_settings.js
@@ -1,13 +1,10 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-    },
     autoplay: {
         delay: 2000,
         enabled: true,
     },
-    speed: 2000,
+    speed: 1500,
 
     // Navigation arrows
     navigation: {
@@ -21,4 +18,78 @@ const swiper = new Swiper('.swiper', {
     },
     observer: true,
     observeParents: true,
+    grabCursor: true,
+});
+
+
+
+const swiper_job = new Swiper('.swiper_job', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        enabled: true,
+    },
+    speed: 500,
+    observer: true,
+    observeParents: true,
+    slidesPerView: 1,
+    grabCursor: true,
+});
+
+
+const swiper_trust_one = new Swiper('.swiper_trust_one', {
+    slidesPerView: 10,
+    loop: true,
+    autoplay: {
+        delay: 1,
+        enabled: true,
+    },
+    speed: 900,
+    grabCursor: true,
+    breakpoints: {
+        100: {
+            slidesPerView: 2
+        },
+        320: {
+            slidesPerView: 3
+        },
+        500: {
+            slidesPerView: 5
+        },
+        700: {
+            slidesPerView: 7
+        },
+        900: {
+            slidesPerView: 10
+        }
+    },
+});
+
+const swiper_trust_two = new Swiper('.swiper_trust_two', {
+    slidesPerView: 10,
+    loop: true,
+    autoplay: {
+        delay: 1,
+        enabled: true,
+        reverseDirection: true,
+    },
+    speed: 2000,
+    grabCursor: true,
+    breakpoints: {
+        100: {
+            slidesPerView: 2
+        },
+        320: {
+            slidesPerView: 3
+        },
+        500: {
+            slidesPerView: 5
+        },
+        700: {
+            slidesPerView: 7
+        },
+        900: {
+            slidesPerView: 10
+        }
+    },
 });
